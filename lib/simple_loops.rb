0d1144fe3f5095a7a70ae4dab347d81969c1eced
distributed-memory-9073 <noahshafer0211@gmail.com>
2020-04-13 23:22:35 +0000

    Done.

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,24 @@
 # Write your methods here
+def loop_message_five_times(msg)
+  count = 0
+  while count != 5
+    puts msg
+    count += 1
+  end
+end
+
+def loop_message_n_times(msg, n)
+  count = 0
+  while count != n
+    puts msg
+    count += 1
+  end  
+end
+
+def output_array(array)
+  array.each {|e| puts e}
+end
+
+def return_string_array(array)
+  array.map {|e| e.to_s}
+end
